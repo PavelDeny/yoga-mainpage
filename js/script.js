0b589bfebe80c80dad1a0baa33f5ec3146ef5cf9
Pavel Denysenko <49881075+PavelDeny@users.noreply.github.com>
2020-06-16 19:33:16 +0300

    added files without index

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 function testWebP(callback) {
-
   var webP = new Image();
   webP.onload = webP.onerror = function () {
     callback(webP.height == 2);
@@ -8,10 +7,50 @@ function testWebP(callback) {
 }
 
 testWebP(function (support) {
-
   if (support == true) {
     document.querySelector('body').classList.add('webp');
-  } else {
+  }
+  else {
     document.querySelector('body').classList.add('no-webp');
   }
 });
+
+$(function () {
+  $('.about__slider').slick({
+    arrows: true,
+    dots: false,
+    infinite: true,
+    fade: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+  });
+});
+
+$(document).ready(function () {
+  $('.header__burger').click(function (event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+
+//$(document).ready(function () {
+//  //Для липкого меню
+//  $(window).scroll(function () {
+//    if ($(this).scrollTop() >= 90) {
+//      $("header").addClass("fix");
+//    } else {
+//      $("header").removeClass("fix");
+//    }
+//  });
+//  //конец липкого меню
+//});
+
+$(function () {
+  $('input, select').styler();
+});
+
+
+//Выпадающее меню на CSS + адаптив под тачскрины
+
+
